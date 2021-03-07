bb7089d9f6cadf3799c80f70da87f20b01c41c38
Fellipe Matos <45209404+fellipematos@users.noreply.github.com>
2021-03-07 15:28:51 -0300

    new script
    Script para Capturar Tela

---
+++ screenshot.py
@@ -0,0 +1,30 @@
+#testado no windows
+#script para capturar ScreenShots
+
+import getpass
+import pyautogui
+import time
+
+
+def screenshot():
+    username = getpass.getuser()
+    ss = 0
+    x = 10 #define quantidade de screenshot
+    
+    while ss < x:
+        #caminho para salvar os screenshot
+        path = f"C:\\Users\{username}\screenshot-{ss}.png"
+        
+        img = pyautogui.screenshot()
+        img.save(path)
+        
+        print(f"Save {path}")#log
+        
+        ss += 1
+        
+        time.sleep(2)#tempo para cada screnshot em segundos
+        
+screenshot()
+print("finished")
+
+    
